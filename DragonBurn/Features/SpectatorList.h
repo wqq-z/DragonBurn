b6890d78ecb6331b408f70671552b7c34a63c1ca
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2024-09-10 17:32:32 +0100

    added speclist

--- DragonBurn/Features/SpectatorList.h
+++ DragonBurn/Features/SpectatorList.h
@@ -7,21 +7,6 @@
 
 namespace SpecList
 {
-    //float spectatorOffsetY = 0;
-    //bool isSpectating(const uint32_t m_hPawn)
-    //{
-    //    uintptr_t pCSPlayerPawn, m_pObserverServices, m_hObserverTarget;
-    //    ProcessMgr.ReadMemory<uintptr_t>(gGame.GetEntityListEntry() + 120 * (m_hPawn & 0x1FF), pCSPlayerPawn);
-    //    ProcessMgr.ReadMemory<uintptr_t>(pCSPlayerPawn + 0x10C0, m_pObserverServices);
-    //    ProcessMgr.ReadMemory<uintptr_t>(m_pObserverServices + 0x44, m_hObserverTarget);
-    //    if (!m_hObserverTarget == 0) {
-    //        //std::cout << m_hObserverTarget << std::endl;
-    //        return true;
-    //    }
-
-    //    return false;
-    //}
-
     void SpectatorWindowList(const CEntity& LocalEntity)
     {
         if (!MiscCFG::SpecList || LocalEntity.Controller.TeamID == 0)
@@ -30,7 +15,7 @@ namespace SpecList
         static float windowWidth = 120.0f;
         ImGuiWindowFlags flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize;
         ImGui::SetNextWindowPos({ 10.0f, ImGui::GetIO().DisplaySize.y / 2 - 200 }, ImGuiCond_Once);
-        ImGui::SetNextWindowSize({ windowWidth, 0 }, ImGuiCond_Once);
+        ImGui::SetNextWindowSize({ windowWidth, (float)LocalEntity.Controller.spectators.size() * 5 });
         ImGui::GetStyle().WindowRounding = 8.0f;
 
         if (ImGui::Begin("Spectators", NULL, flags))
@@ -41,6 +26,7 @@ namespace SpecList
 
             for (const auto& spectator : LocalEntity.Controller.spectators)
             {
+                ImGui::SetCursorPosY(ImGui::GetCursorPosX() + 5);
                 ImGui::TextColored(ImColor(0, 255, 0, 255), spectator.c_str());
                 ImGui::SetCursorPosY(ImGui::GetCursorPosY() + lineSpacing);
             }
@@ -50,53 +36,58 @@ namespace SpecList
         }
     }
 
-    void GetSpectatorList(CEntity& Entity, CEntity& LocalEntity)
+    uintptr_t getAddressBase(uintptr_t entityList, uintptr_t playerPawn)
+    {
+        uintptr_t listEntrySecond;
+        ProcessMgr.ReadMemory<uintptr_t>(entityList + 0x8 * ((playerPawn & 0x7FFF) >> 9) + 16, listEntrySecond);
+
+        uintptr_t isPawn;
+        ProcessMgr.ReadMemory<uintptr_t>(listEntrySecond + 120 * (playerPawn & 0x1FF), isPawn);
+
+        return listEntrySecond == 0 ? 0 : isPawn;
+    }
+
+    void GetSpectatorList(CEntity Entity, CEntity& LocalEntity)
     {
         if (!MiscCFG::SpecList || LocalEntity.Controller.TeamID == 0)
             return;
 
-        //uintptr_t LocalPlayer = MemMan.ReadMem<uintptr_t>(baseAddy + offsets::clientDLL["dwLocalPlayerController"]);
-        //uintptr_t localPlayerPawn = MemMan.ReadMem<uintptr_t>(LocalPlayer + clientDLL::clientDLLOffsets["CBasePlayerController"]["fields"]["m_hPawn"]);
-        //uintptr_t list_entry2 = MemMan.ReadMem<uintptr_t>(entityList + 0x8 * ((localPlayerPawn & 0x7FFF) >> 9) + 16);
-        //if (!list_entry2)
-        //    return false;
+        LocalEntity.Controller.spectators.empty();
+        std::cout << Entity.Controller.PlayerName << '\n';
 
-        //const uintptr_t CSlocalPlayerPawn = MemMan.ReadMem<uintptr_t>(list_entry2 + 120 * (localPlayerPawn & 0x1FF));
+        uintptr_t LocalPlayer;
+        ProcessMgr.ReadMemory<uintptr_t>(gGame.GetClientDLLAddress() + Offset.LocalPlayerController, LocalPlayer);
+        uintptr_t localPlayerPawn;
+        ProcessMgr.ReadMemory<uintptr_t>(LocalPlayer + Offset.PlayerController.m_hPawn, localPlayerPawn);
 
-        //if (localPlayer)
-        //    return this->getSpectating() == CSlocalPlayerPawn;
-        //return this->getSpectating() != 0;
+        uintptr_t CSlocalPlayerPawn;
+        ProcessMgr.ReadMemory<uintptr_t>(gGame.GetEntityListEntry() + 120 * (localPlayerPawn & 0x1FF), CSlocalPlayerPawn);
 
-        // LocalEntity.Controller.spectators.push_back(Entity.Controller.PlayerName);
+        uint32_t spectatorPawn;
+        ProcessMgr.ReadMemory<uint32_t>(Entity.Controller.Address + Offset.PlayerController.m_hPawn, spectatorPawn);
 
-        //LocalEntity.Controller.spectators.empty();
-        //uint32_t m_hPawn;
-        //uintptr_t pCSPlayerPawn;
-        //uintptr_t m_pObserverServices;
+        uintptr_t entityList;
+        ProcessMgr.ReadMemory<uintptr_t>(gGame.GetEntityListAddress(),entityList);
 
-        //ProcessMgr.ReadMemory<uint32_t>(Entity.Controller.Address + 0x5DC, m_hPawn);
-        //ProcessMgr.ReadMemory<uintptr_t>(gGame.GetEntityListEntry() + 120 * (m_hPawn & 0x1FF), pCSPlayerPawn);
-        //ProcessMgr.ReadMemory<uintptr_t>(pCSPlayerPawn + 0x10C0, m_pObserverServices);
-        //std::cout << "SL" << '\n';
+        uintptr_t pawn = getAddressBase(entityList, spectatorPawn);
 
-        //if (m_pObserverServices)
-        //{
-        //    std::cout << "Observed" << '\n';
-        //    std::cout << LocalEntity.Pawn.Address << '\n';
-        //    uint32_t m_hObserverTarget;
-        //    uintptr_t list_entry;
-        //    uintptr_t pController;
-        //    ProcessMgr.ReadMemory<uint32_t>(m_pObserverServices + 0x44, m_hObserverTarget);
-        //    ProcessMgr.ReadMemory<uintptr_t>(EntityAddress + 0x8 * ((m_hObserverTarget & 0x7FFF) >> 9) + 0x10, list_entry);
-        //    ProcessMgr.ReadMemory<uintptr_t>(gGame.GetEntityListEntry() + 120 * (m_hObserverTarget & 0x1FF), pController);
+        uintptr_t observed;
+        ProcessMgr.ReadMemory<uintptr_t>(pawn + Offset.PlayerController.m_pObserverServices, observed);
+        std::cout << "observed: " << observed << '\n';
 
-        //    std::cout << "Player: " << Entity.Controller.PlayerName << " m_hObserverTarget: " << m_hObserverTarget << "list_entry: " << list_entry << "pController: " << pController << '\n';
+        uint64_t observedTarget;
+        ProcessMgr.ReadMemory<uintptr_t>(observed + Offset.PlayerController.m_hObserverTarget, observedTarget);
+        std::cout << "observedTarget: " << observedTarget << '\n';
 
-        //    if (pController == LocalEntity.Pawn.Address)
-        //    {
-        //        LocalEntity.Controller.spectators.push_back(Entity.Controller.PlayerName);
-        //        std::cout << Entity.Controller.PlayerName << '\n';
-        //    }
-        //}
+        uintptr_t spectatorTarget = getAddressBase(entityList, observedTarget);
+        std::cout << "handle: " << spectatorTarget << '\n';
+        if (observed)
+        {
+            if (spectatorTarget == CSlocalPlayerPawn)
+            {
+                LocalEntity.Controller.spectators.push_back(Entity.Controller.PlayerName);
+            }
+        }
+        std::cout << "\n\n\n";
     }
 }
