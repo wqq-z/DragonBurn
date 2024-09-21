22e4adb2d0b955c36214e42b75c3d04d3ec7f7d2
ByteCorum <164874887+ByteCorum@users.noreply.github.com>
2024-09-21 16:24:29 +0300

    support of all symbols

--- DragonBurn/Resources/Language.hpp
+++ DragonBurn/Resources/Language.hpp
@@ -26,13 +26,13 @@ namespace Text {
         inline std::string Distance = "Distance";
         inline std::string PlayerName = "Player Name";
         inline std::string SnapLine = "Snap Line";
-        inline std::string LinePosList = "Line Position: ";
+        inline std::string LinePosList = "   Line Position";
         inline std::string VisCheck = "Visible Color";
         inline std::string Preview = "ESP Preview";
         inline std::string MultiColor = "Multicoloured";
         inline std::string MultiColTip = "Only work when the box has non-rounded corners.";
         inline std::string Outline = "Outline";
-        inline std::string BoxType = "  Box Type";
+        inline std::string BoxType = "   Box Type";
         inline std::string HealthNum = "Health Number";
         inline std::string Ammo = "Ammo Bar";
         inline std::string ScopedESP = "Show Scoped";
@@ -44,7 +44,7 @@ namespace Text {
     {
         inline std::string Enable = "Enable Aimbot";
         inline std::string FeatureName = "Aimbot";
-        inline std::string HotKeyList = "Hotkey   ";
+        inline std::string HotKeyList = "Hotkey";
         inline std::string Toggle = "Toggle Mode";
         inline std::string DrawFov = "Show Fov";
         inline std::string VisCheck = "Visible Check";
@@ -83,7 +83,7 @@ namespace Text {
     {
         inline std::string Enable = "Enable Triggerbot";
         inline std::string FeatureName = "Triggerbot";
-        inline std::string HotKeyList = "Hotkey   ";
+        inline std::string HotKeyList = "Hotkey";
         inline std::string Toggle = "Always Active";
         inline std::string ScopeOnly = "Scope Only";
         inline std::string DelaySlider = "Shot Delay:";
